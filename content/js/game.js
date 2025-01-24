780d79f6af90a71ea68b72235ec2a7a937d633e3
Alpha-TesterMesser <computerkingang@gmail.com>
2025-01-24 11:03:19 -0800

    added games.js

---
+++ content/js/game.js
@@ -0,0 +1,30 @@
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+fetch("/content/json/games.json").then((res) => res.json()).then(data => {
+    users = data.map(user=> {
+        const card = userCardTemplate.content.cloneNode(true).children[0]
+        const header = card.querySelector("[data-header]")
+
+        // adding content into the query selected i.e header and body
+        header.textContent = user.title
+        //card.textContent = user.title
+        card.href = user.link
+        userCardContainer.append(card)
+
+        // return the result after successfull fetch
+        return {title: user.title, description: user.link, element: card}
+    });
+
+})
+
+searchInput.addEventListener('input', (e) => {
+    const value = e.target.value
+    users.forEach((user) => {
+        const isVisible = user.title.toLowerCase().includes(value)
+        user.element.classList.toggle('hide', !isVisible)
+    })
+})
