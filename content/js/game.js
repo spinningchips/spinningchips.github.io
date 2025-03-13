af08be7e754f45c8f77c48f59e23fee4e7a03828
Mr-milky-way <131718510+Mr-milky-way@users.noreply.github.com>
2025-03-13 18:01:54 +0000

    test

--- content/js/game.js
+++ content/js/game.js
@@ -25,6 +25,21 @@ function getCookie(cname) {
     return "";
 }
 
+function checkCookie() {
+    let url = getCookie("url");
+    if (url != "") {
+        alert("Going to " + url);
+        document.getElementById('game').width = 1000;
+        document.getElementById('game').height = 600;
+        document.getElementById('game').src = url;
+    } else {
+        url = prompt("Please enter your url:", "");
+        if (url != "" && url != null) {
+            setCookie("url", url, 365);
+        }
+    }
+}
+
 fetch("/content/json/games.json").then((res) => res.json()).then(data => {
     users = data.map(user=> {
         const card = userCardTemplate.content.cloneNode(true).children[0]
@@ -35,7 +50,7 @@ fetch("/content/json/games.json").then((res) => res.json()).then(data => {
         header.textContent = user.title
         //card.textContent = user.title
         card.href = user.link
-        card.onClick = "setCookie(url, user.link, 365);"
+        card.onClick = "checkCookie();"
         img.src = user.image
         userCardContainer.append(card)
 
